312a1e605fa49ca28049f9223a7239c9912184f4
yamacir-kit <httperror@404-notfound.jp>
2017-04-11 06:36:18 +0900

    Fix script

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -24,37 +24,37 @@ echo "export JAVA_HOME=`readlink -f /usr/bin/javac | sed -e 's:/bin/javac::'`" >
 echo "export HADOOP_HOME='/usr/local/hadoop'" >> ~/.bashrc
 echo 'export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
 
-hadoop_home="$install_prefix/hadoop"
-sudo cp $script_path/configs/* $hadoop_home/etc/hadoop/
-
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+ssh-copy-id localhost
 
-sudo su
-echo "ssh" > /etc/pdsh/rcmd_default
-exit
+echo "ssh" > rcmd_default
+sudo mv rcmd_default /etc/pdsh/
 
+hadoop_home="$install_prefix/hadoop"
 pushd $hadoop_home
 bin/hdfs namenode -format
 popd
 
 echo ""
 echo "How to run a MapReduce job locally"
-echo "  1. mv $hadoop_home"
-echo "  2. sbin/start-dfs.sh"
-echo "  3. bin/hdfs dfs -mkdir hdfs://localhost:9000/input"
-echo "  5. bin/hdfs dfs -mkdir -p hdfs://localhost:9000/output/grep"
-echo "  4. bin/hdfs dfs -put etc/hadoop/*.xml hdfs://localhost:9000/input"
-echo "  6. bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-$version.jar grep hdfs://localhost:9000/input hdfs://localhost:9000/output 'dfs[a-z.]+'"
-echo "  7. bin/hdfs dfs -cat output/*"
-echo "  8. sbin/stop-dfs.sh"
+echo "  1. cd $hadoop_home"
+echo "  2. mkdir input"
+echo "  3. cp etc/hadoop/*.xml input/"
+echo "  4. hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-$version.jar wordcount input output"
 
 echo ""
 echo "How to run a MapReduce job on YARN in a pseudo-distoributed mode"
-echo "  1. sbin/start-yarn.sh"
-echo "  2. Run a MapReduce job"
-echo "  3. sbin/stop-yarn.sh"
+echo "  1. cp $script_path/configs_for_pseudo/* $hadoop_home/etc/hadoop/"
+echo "  2. cd $hadoop_home"
+echo "  3. hdfs namenode -format"
+echo "  4. start-all.sh"
+echo "  5. hdfs dfs -mkdir -p /user/`id -un`"
+echo "  6. hdfs dfs -put etc/hadoop input"
+echo "  7. hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-$version.jar grep input output 'dfs[a-z.]+'"
+echo "  8. hdfs dfs -cat output/*"
+echo "  9. stop-all.sh"
 
 # echo `java -version 2>&1 /dev/null | grep --color=none -e 'version' | sed -e 's/^openjdk version "\(.*\)"$/\1/g'`
 
