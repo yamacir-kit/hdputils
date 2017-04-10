16a9bf4e231df8f915178e613f9427cbebf43979
yamacir-kit <httperror@404-notfound.jp>
2017-04-10 20:49:08 +0900

    Fix script quotation miss

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -21,12 +21,12 @@ popd
 
 echo "export JAVA_HOME='/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/jre'" >> ~/.bashrc
 echo "export HADOOP_HOME='/usr/local/hadoop'" >> ~/.bashrc
-echo "export HADOOP_INSTALL=$HADOOP_HOME" >> ~/.bashrc
-echo "export HADOOP_MAPRED_HOME=$HADOOP_HOME" >> ~/.bashrc
-echo "export HADOOP_COMMON_HOME=$HADOOP_HOME" >> ~/.bashrc
-echo "export HADOOP_COMMON_LIB_NATIVE_DIR="$HADOOP_HOME/lib/native"" >> ~/.bashrc
-echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" >> ~/.bashrc
-echo "export YARN_HOME=$HADOOP_HOME" >> ~/.bashrc
+echo 'export HADOOP_INSTALL=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_COMMON_LIB_NATIVE_DIR="$HADOOP_HOME/lib/native"' >> ~/.bashrc
+echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export YARN_HOME=$HADOOP_HOME' >> ~/.bashrc
 echo 'export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
 
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
